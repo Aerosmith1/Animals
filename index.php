33d78ddbc839a900788968139fe9cca48f5399b2
Ara <arastepanyan20@gmail.com>
2019-08-24 23:57:18 +0500

    HTML of webpage

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head> 
+ <link rel="stylesheet" href="styles/style.css" 
+ type="text/css" >
+	<title></title>
+	
+
+</head>
+<body >
+
+	<header>
+		<a href="/project/PHPproject/" title="Return to home">
+		<img src="img/banner.png" ></a>		
+     </header> 
+	
+<nav>     
+<ul>
+	
+<li><a href="index.php">Home</a></li>
+<li><a href="team.php">Team</a></li>
+<li><a href="menu.php">Menu</a></li>
+<li><a href="contact.php">Contact</a></li>
+
+</ul>
+
+
+</nav>
+	
+<main>
+	
+
+
+
+</main>
+
+<footer>
+	
+<div class="column three"> <strong>phone</strong>
+255-325-645
+
+ </div>
+ <!-- column	 -->
+<div class="column three"><strong>location</strong>
+Mamikonyants 36/2
+</div>
+<!-- column -->
+
+<small>&copy;2019 Ara Stepanyan</small>
+
+</footer>
+
+
+
+</body>
+</html>
